51cff2997144c33a8be6719a569e670583fec11e
ranandasatria <rananda24@gmail.com>
2025-07-20 22:58:37 +0700

    feat: add adminCheck middleware on /admin/genres endpoints

--- src/routers/genre.router.js
+++ src/routers/genre.router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const genreController = require('../controllers/genre.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+const adminCheck = require('../middlewares/admin.middleware');
 
 router.get('/', genreController.getAllGenres);
-
-router.post('/', authMiddleware, genreController.createGenre);
+router.post('/', authMiddleware, adminCheck, genreController.createGenre);
 
 module.exports = router;
