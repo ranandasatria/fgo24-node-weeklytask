bab161ab4dde86fdbaa5eb0ba15775341b68196a
ranandasatria <rananda24@gmail.com>
2025-07-20 23:02:03 +0700

    feat: add adminCheck middleware on /admin/payment-method endpoints

--- src/routers/paymentmethod.router.js
+++ src/routers/paymentmethod.router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const paymentMethodController = require('../controllers/paymentmethod.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+const adminCheck = require('../middlewares/admin.middleware');
 
 router.get('/', paymentMethodController.getAllPaymentMethods)
-router.post('/', authMiddleware,  paymentMethodController.createPaymentMethod);
+router.post('/', authMiddleware,  adminCheck, paymentMethodController.createPaymentMethod);
 
 module.exports = router
