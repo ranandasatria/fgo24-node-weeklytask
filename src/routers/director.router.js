8684c0fa184cfd5b5c325ae9b490483453e17691
ranandasatria <rananda24@gmail.com>
2025-07-20 22:58:55 +0700

    feat: add adminCheck middleware on /admin/directors endpoints

--- src/routers/director.router.js
+++ src/routers/director.router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const directorController = require('../controllers/director.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
+const adminCheck = require('../middlewares/admin.middleware');
 
 router.get('/', directorController.getAllDirectors)
-
-router.post('/', authMiddleware,  directorController.createDirector);
+router.post('/', authMiddleware,  adminCheck, directorController.createDirector);
 
 module.exports = router;
